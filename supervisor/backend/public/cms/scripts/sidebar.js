81b0c855da84fb21e1b5665d0115a2a39b7497d2
Subraiz <subraizahmed@gmail.com>
2019-07-23 21:19:49 -0400

    Update UI

---
+++ supervisor/backend/public/cms/scripts/sidebar.js
@@ -0,0 +1,47 @@
+let navigationLinks = document.querySelectorAll(".navigation-link");
+let websiteBaseURL =
+  `/cms/` + document.querySelector("#websiteID").getAttribute("data-websiteID");
+
+navigationLinks.forEach(link => {
+  let pageLink = link.getAttribute("href");
+  link.addEventListener("click", event => {
+    event.preventDefault();
+    let url = window.location.origin + `${websiteBaseURL}` + `/${pageLink}`;
+    let parentDiv = getClosest(link, ".sidebar-item");
+    let icons = document.querySelectorAll(".sidebar-item");
+    icons.forEach(icon => {
+      if (icon.classList.contains("active")) {
+        icon.classList.remove("active");
+      }
+    });
+    parentDiv.classList.add("active");
+
+    setTimeout(() => {
+      window.location.href = url;
+    }, 350);
+  });
+});
+
+var getClosest = function(elem, selector) {
+  // Element.matches() polyfill
+  if (!Element.prototype.matches) {
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector ||
+      Element.prototype.oMatchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      function(s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+          i = matches.length;
+        while (--i >= 0 && matches.item(i) !== this) {}
+        return i > -1;
+      };
+  }
+
+  // Get the closest matching element
+  for (; elem && elem !== document; elem = elem.parentNode) {
+    if (elem.matches(selector)) return elem;
+  }
+  return null;
+};
