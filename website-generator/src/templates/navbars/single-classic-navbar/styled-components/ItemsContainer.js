7aa88dd144cf412a7d8c26e795c30935ecc604ba
Subraiz <subraizahmed@gmail.com>
2019-07-28 08:20:12 -0400

    Fixed navbar

--- website-generator/src/templates/navbars/single-classic-navbar/styled-components/ItemsContainer.js
+++ website-generator/src/templates/navbars/single-classic-navbar/styled-components/ItemsContainer.js
@@ -10,7 +10,8 @@ const ItemsContainer = styled.div`
     transition: 0.3s linear;
     flex-flow: column;
     width: 45vw;
-    justify-content: center;
+    justify-content: flex-start;
+    padding-top: 15px;
     margin-top: 29px;
     box-shadow: 3px 20px 12px 4px rgba(0, 0, 0, 0.2);
     position: absolute;
